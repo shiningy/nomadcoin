9cafd886ad123b99f61118516dd83cc2048a39f0
myunghyun_yang <shiningy84@gmail.com>
2021-12-27 22:26:46 +0900

    7.1 Parsing Commands

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"github.com/shiningy/nomadcoin/explorer"
-	"github.com/shiningy/nomadcoin/rest"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcome to 노마드 코인\n\n")
+	fmt.Printf("Please use the follwing commands:\n\n")
+	fmt.Printf("explorer: 	Start the HTML Explorer\n\n")
+	fmt.Printf("rest: 			Start the REST API (recommended\n\n")
+	os.Exit(0)
+}
+
 func main() {
-	go explorer.Start(3000)
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
 }
