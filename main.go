95d79e4a16750ef52a3fe8577a5a070b1fb838fd
myunghyun_yang <shiningy84@gmail.com>
2021-12-27 11:26:12 +0900

    4.5 Refactoring part Two

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-import "github.com/shiningy/nomadcoin/blockchain"
+import (
+	"fmt"
 
-
+	"github.com/shiningy/nomadcoin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+	}
 }
