bddc550c05bb1e6ad27a3e17755fb327bf08a4f2
myunghyun_yang <shiningy84@gmail.com>
2021-12-27 22:45:38 +0900

    7.3 Flag

---
+++ cli/cli.go
@@ -0,0 +1,38 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/shiningy/nomadcoin/explorer"
+	"github.com/shiningy/nomadcoin/rest"
+)
+
+func usage() {
+	fmt.Printf("Welcome to 노마드 코인\n\n")
+	fmt.Printf("Please use the follwing flags:\n\n")
+	fmt.Printf("-port: 		Set the PORT of the server\n")
+	fmt.Printf("-mode:		Choose between 'html' and 'rest'\n\n")
+	os.Exit(0)
+}
+
+func Start() {
+	if len(os.Args) == 1 {
+		usage()
+	}
+
+	port := flag.Int("port", 4000, "Sets the port of the server (default 4000)")
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
+
+	flag.Parse()
+
+	switch *mode {
+	case "rest":
+		rest.Start(*port)
+	case "html":
+		explorer.Start(*port)
+	default:
+		usage()
+	}
+}
--- main.go
+++ main.go
@@ -1,39 +1,9 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
+	"github.com/shiningy/nomadcoin/cli"
 )
 
-func usage() {
-	fmt.Printf("Welcome to 노마드 코인\n\n")
-	fmt.Printf("Please use the follwing commands:\n\n")
-	fmt.Printf("explorer: 	Start the HTML Explorer\n\n")
-	fmt.Printf("rest: 			Start the REST API (recommended\n\n")
-	os.Exit(0)
-}
-
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-	}
-
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-
-	portFlag := rest.Int("port", 4000, "Sets the port of the server (default 4000)")
-
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start Explorer")
-	case "rest":
-		rest.Parse(os.Args[2:])
-	default:
-		usage()
-	}
-
-	if rest.Parsed() {
-		fmt.Println(*portFlag)
-		fmt.Println("Start server")
-	}
+	cli.Start()
 }
