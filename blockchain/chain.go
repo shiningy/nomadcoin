1deccdaaccfaa3ae531176c01b2510c333f1b1ec
myunghyun_yang <shiningy84@gmail.com>
2021-12-28 14:53:05 +0900

    8.5 Restoring the Blockchain

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -1,7 +1,13 @@
 package blockchain
 
 import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
 	"sync"
+
+	"github.com/shiningy/nomadcoin/db"
+	"github.com/shiningy/nomadcoin/utils"
 )
 
 type blockchain struct {
@@ -12,18 +18,36 @@ type blockchain struct {
 var b *blockchain
 var once sync.Once
 
+func (b *blockchain) restore(data []byte) {
+	utils.HandleErr(gob.NewDecoder(bytes.NewReader(data)).Decode(b))
+}
+
+func (b *blockchain) persist() {
+	db.SaveBlockchain(utils.ToBytes(b))
+}
+
 func (b *blockchain) AddBlock(data string) {
-	block := createBlock(data, b.NewestHash, b.Height)
+	block := createBlock(data, b.NewestHash, b.Height+1)
 	b.NewestHash = block.Hash
 	b.Height = block.Height
+	b.persist()
 }
 
 func Blockchain() *blockchain {
 	if b == nil {
 		once.Do(func() {
 			b = &blockchain{"", 0}
+			fmt.Printf("NewestHash: %s\nHeight:%d\n", b.NewestHash, b.Height)
+			checkpoint := db.Checkpoint()
+			if checkpoint == nil {
+				b.AddBlock("Genesis")
+			} else {
+				fmt.Println("Restoring...")
+				b.restore(checkpoint)
+			}
 			b.AddBlock("Genesis")
 		})
 	}
+	fmt.Printf("NewestHash: %s\nHeight:%d\n", b.NewestHash, b.Height)
 	return b
 }
