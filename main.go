3e5f754b18b477190fda7b50ad2083b26c8cdb23
myunghyun_yang <shiningy84@gmail.com>
2021-12-30 13:24:42 +0900

    9.2 Mine Block

--- main.go
+++ main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
+	"github.com/shiningy/nomadcoin/cli"
+	"github.com/shiningy/nomadcoin/db"
 )
 
 func main() {
-	difficulty := 6
-	target := strings.Repeat("0", difficulty)
-	nonce := 1
-	for {
-		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
-		fmt.Printf("Hash:%s\nTarget:%s\nNonce:%d\n\n", hash, target, nonce)
-		if strings.HasPrefix(hash, target) {
-			return
-		} else {
-			nonce++
-		}
-	}
+	defer db.Close()
+	cli.Start()
 }
