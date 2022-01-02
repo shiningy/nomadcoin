99d3d0d83a680e3e5db4ee44ef1d5187aa42112c
myunghyun_yang <shiningy84@gmail.com>
2022-01-02 19:04:24 +0900

    12.1 Why Go Routines

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	"github.com/shiningy/nomadcoin/cli"
-	"github.com/shiningy/nomadcoin/db"
+	"fmt"
+	"time"
 )
 
+func countToTen(name string) {
+	for i := range [10]int{} {
+		fmt.Println(i, name)
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
-	defer db.Close()
-	cli.Start()
+	go countToTen("first")
+	go countToTen("second")
+	for {
+
+	}
 }
