8f2c0ab7df0dcd44358fbb83052cddcaae8a8bd0
myunghyun_yang <shiningy84@gmail.com>
2022-01-09 01:17:39 +0900

    13.13 Block Test Part One

---
+++ blockchain/chain_test.go
@@ -0,0 +1,52 @@
+package blockchain
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/shiningy/nomadcoin/utils"
+)
+
+type fakeDB struct {
+	fakeLoadChain func() []byte
+	fakeFindBlock func() []byte
+}
+
+func (f fakeDB) FindBlock(hash string) []byte {
+	return f.fakeFindBlock()
+}
+
+func (f fakeDB) LoadChain() []byte {
+	return f.fakeLoadChain()
+}
+
+func (fakeDB) SaveBlock(hash string, data []byte) {}
+func (fakeDB) SaveChain(data []byte)              {}
+func (fakeDB) DeleteAllBlocks()                   {}
+
+func TestBlockchain(t *testing.T) {
+	t.Run("Should create blockchain", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeLoadChain: func() []byte {
+				return nil
+			},
+		}
+		bc := Blockchain()
+		if bc.Height != 1 {
+			t.Error("Blockchain() should create a blockchain.")
+		}
+	})
+	t.Run("Should restore blockchain", func(t *testing.T) {
+		once = *new(sync.Once)
+		dbStorage = fakeDB{
+			fakeLoadChain: func() []byte {
+				bc := &blockchain{Height: 2, NewestHash: "xxx", CurrentDifficulty: 1}
+				return utils.ToBytes(bc)
+			},
+		}
+		bc := Blockchain()
+		if bc.Height != 2 {
+			t.Errorf("Blockchain() should restore a blockchain with a height of %d, got %d", 2, bc.Height)
+		}
+	})
+}
